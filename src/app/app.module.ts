fd6f59ba7e9e35e415614d0050dfd3b233cff411
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-15 16:51:21 +0100

    update declarations in app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import { ListHuntingComponent } from './components/hunting/list-hunting/list-hun
 import { CreateHuntingComponent } from './components/hunting/create-hunting/create-hunting.component';
 import { UpdateHuntingComponent } from './components/hunting/update-hunting/update-hunting.component';
 import { ViewHuntingComponent } from './components/hunting/view-hunting/view-hunting.component';
+import { ListLevelComponent } from './components/level/list-level/list-level.component';
+import { CreateLevelComponent } from './components/level/create-level/create-level.component';
+import { UpdateLevelComponent } from './components/level/update-level/update-level.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,10 @@ import { ViewHuntingComponent } from './components/hunting/view-hunting/view-hun
     ListHuntingComponent,
     CreateHuntingComponent,
     UpdateHuntingComponent,
-    ViewHuntingComponent
+    ViewHuntingComponent,
+    ListLevelComponent,
+    CreateLevelComponent,
+    UpdateLevelComponent
   ],
   imports: [
     BrowserModule,

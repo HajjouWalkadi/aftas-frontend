69d88a4efe77e81208eac72b82f3bb62d36d8a08
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-15 16:59:48 +0100

    Services for levels are added

---
+++ src/app/services/level.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Level } from '../models/level';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LevelService {
+
+  private baseUrl = 'http://localhost:8080/api/v1/levels';
+
+  constructor(private http: HttpClient) { }
+
+  getAllLevels(): Observable<Level[]> {
+    return this.http.get<Level[]>(this.baseUrl);
+  }
+
+  getLevelById(id: number): Observable<Level> {
+    return this.http.get<Level>(`${this.baseUrl}/${id}`);
+  }
+
+  saveLevel(level: Level): Observable<Level> {
+    return this.http.post<Level>(this.baseUrl, level);
+  }
+}

26aa36b660603731a295372ac331417a6ad6c889
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-15 16:17:20 +0100

    fish and competition routing are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListCompetitionComponent } from './components/competition/list-competition/list-competition.component';
+import { CreateCompetitionComponent } from './components/competition/create-competition/create-competition.component';
+import { UpdateCompetitionComponent } from './components/competition/update-competition/update-competition.component';
+import { ListFishComponent } from './components/fish/list-fish/list-fish.component';
+import { CreateFishComponent } from './components/fish/create-fish/create-fish.component';
+import { UpdateFishComponent } from './components/fish/update-fish/update-fish.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'competitions', component: ListCompetitionComponent },
+  { path: 'competitions/create', component: CreateCompetitionComponent },
+  { path: 'competitions/update', component: UpdateCompetitionComponent },
+  { path: 'fish', component: ListFishComponent },
+  { path: 'fish/create', component: CreateFishComponent },
+  { path: 'fish/update', component: UpdateFishComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

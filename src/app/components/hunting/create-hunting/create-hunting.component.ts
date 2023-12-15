0e1d53d70af07ae2dc27e54c540d9f66949afa1d
hajjou walkadi <walkadihajjou153@gmail.com>
2023-12-15 16:14:03 +0100

    create hunting components

---
+++ src/app/components/hunting/create-hunting/create-hunting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-hunting',
+  templateUrl: './create-hunting.component.html',
+  styleUrls: ['./create-hunting.component.css']
+})
+export class CreateHuntingComponent {
+
+}
